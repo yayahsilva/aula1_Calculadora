570385545d9b1126ef8f709fb2664b57fa481efb
yayahsilva <mayara.franciele39@gmail.com>
2022-07-08 16:59:54 -0300

    first commit

---
+++ src/App.java
@@ -0,0 +1,12 @@
+public class App {
+    public static void main(String[] args) {
+        int x = 5;
+        int y = 10;
+        int resultado;
+
+        resultado = x + y;
+       
+        System.out.println("Resultado: "+ resultado);
+    }
+   
+}
